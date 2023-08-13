6b4afef652134a2daa24e793456be1df4f31ee72
Stanislav Ishkov <htrix15@gmail.com>
2023-08-13 15:29:07 +0300

    Upgrade for search by many intervals

--- Billboards/Common/Event/Event.cs
+++ Billboards/Common/Event/Event.cs
@@ -1,9 +1,11 @@
 ﻿using Playbill.Billboards.Common.Enums;
+using Playbill.Common;
 
 namespace Playbill.Billboards.Common.Event;
 
 public class Event
 {
+    public required BillboardTypes Billboard { get; set; }
     public required EventTypes Type { get; set; }
     public required DateTime Date { get; set; }
     public required string Title { get; set; }
