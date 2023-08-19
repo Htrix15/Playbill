69920cd92c289dc2e61f0e6a962523ff1471ced1
Stanislav Ishkov <htrix15@gmail.com>
2023-08-19 17:56:51 +0300

    Remove samples

--- Billboards/Sample/SamplePageParseOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿using Playbill.Billboards.Common.Enums;
-using Playbill.Billboards.Common.Options;
-
-namespace Playbill.Billboards.Sample;
-
-public class SamplePageParseOptions : PageParseOptions
-{
-}
